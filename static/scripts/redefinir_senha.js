8732af9e423ff134ca2f8de4001cc2b65d6c8d82
Ana Clara <anaclara.gomes@lasalle.org.br>
2024-05-27 18:28:44 -0300

    Adicionando botão de voltar na página redefinir_senha.html com html, css e js

--- static/scripts/redefinir_senha.js
+++ static/scripts/redefinir_senha.js
@@ -1,6 +1,6 @@
 let titulo = document.querySelector('#titulo')
 let subtitulo = document.querySelector('#subtitulo')
-let btnAvancar = document.querySelector('#avancar')
+let btnAvancar = document.querySelector('#btn-avancar')
 let textoBtnAvancar = document.querySelector('#texto_botao')
 let divInput = document.querySelector('.input')
 let divAutenticacao = document.querySelector('.autenticacao')
@@ -8,7 +8,42 @@ let divNovaSenha = document.querySelector('.nova-senha')
 let divRespostaSenha = document.querySelector('.resposta-senha')
 let divTitulo = document.querySelector('#redefinicao-senha .topo')
 let email = 'contact@dscode...com'
+let btnVoltar = document.querySelector('#btn-voltar')
 
+// Adiciona um listener de evento para o botão de voltar, permitindo retornar ao passo anterior do processo de redefinição de senha
+btnVoltar.addEventListener('click', function (event) {
+    if(textoBtnAvancar.innerText === 'Receber código') {
+        setTimeout(function() {
+            window.location.href = '/area_restrita'
+        }, 10)  
+    }
+
+    if(textoBtnAvancar.innerText === 'Verificar código') {
+        divAutenticacao.classList.toggle('off') // fica off
+        divInput.classList.toggle('off') // fica on
+
+        titulo.innerText = 'Esqueceu a senha?'
+        subtitulo.innerHTML = `Por favor, informe o seu email para receber um código e redefinir a sua senha.`
+
+        setTimeout(function() {
+            textoBtnAvancar.innerText = 'Receber código'
+        }, 10)
+    }
+
+    if(textoBtnAvancar.innerText === 'Alterar senha') {
+        divNovaSenha.classList.toggle('off') // fica off
+        divAutenticacao.classList.toggle('off') // fica on
+
+        titulo.innerText = 'Verifique seu email'
+        subtitulo.innerHTML = `Enviamos um email de redefinição para <strong>${email}</strong>. Insira o código de 5 dígitos mencionado no email.`
+
+        setTimeout(function() {
+            textoBtnAvancar.innerText = 'Verificar código'
+        }, 10)
+    }
+})
+
+// Adiciona um listener de evento para o botão de avançar, permitindo avançar no processo de redefinição de senha
 btnAvancar.addEventListener('click', function (event) {
     event.preventDefault()
 
